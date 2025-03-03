57e182a77a1efa8538101f68335d6d5c61434947
Duy Nguyễn <yudnguyen2005@gmail.com>
2025-03-04 02:11:55 +0700

    Day05

---
+++ Day05/nqd-lab05/src/NqdApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NqdApp extends Component {
+  render() {
+    return (
+      <div className="container border mt-3">
+          <h1 className= 'text-center alert alert-info my-2'>K23CNT2 - Nguyễn Quốc Duy - Event And Form</h1>
+          <hr/>
+      </div>
+    );
+  }
+}
+
+export default NqdApp; 
