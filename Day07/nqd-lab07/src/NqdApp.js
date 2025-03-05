5d33a5a2fc44bfd4df50558997a867db24f6e091
Duy Nguyễn <yudnguyen2005@gmail.com>
2025-03-05 13:36:25 +0700

    Day07

--- Day07/nqd-lab07/src/NqdApp.js
+++ Day07/nqd-lab07/src/NqdApp.js
@@ -7,7 +7,7 @@ class NqdApp extends Component {
   render() {
     return (
       <div className='conrainer border mt-3'>
-        <h1 className='text-center'>K23CNT2 - Nguyễn Quốc Duy - Render condition, List Key</h1>
+        <h1 className='text-center'>Nguyễn Quốc Duy - K23CNT2 - Render condition, List Key</h1>
         <hr/>
         <NqdLoginControl />
         <NqdRenderList />
